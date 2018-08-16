c090929930fa857b3a9710579dc307ae5ad0a0a3
buyanxia <18842690392@163.com>
2018-08-16 10:34:26 +0800

    add echart and tree component api

--- src/libs/api.js
+++ src/libs/api.js
@@ -8,3 +8,31 @@ export function getAuthorizedMenus () {
     method: 'get'
   })
 }
+
+export function getHebeiData () {
+  return fetch({
+    url: apiPrefix + '/w/hebei',
+    method: 'get'
+  })
+}
+
+export function getOrganization () {
+  return fetch({
+    url: apiPrefix + '/w/organization',
+    method: 'get'
+  })
+}
+
+export function getTreeTableList () {
+  return fetch({
+    url: apiPrefix + '/w/table',
+    method: 'get'
+  })
+}
+
+export function getTreeTableDetail (id) {
+  return fetch({
+    url: apiPrefix + '/w/table/' + id,
+    method: 'get'
+  })
+}
